3cfad5a1c567b1ad141129ce0d026e4e2e2dd229
LinkKagenou <163136696+LinkKagenou@users.noreply.github.com>
2024-03-12 09:39:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://horadafofoca.com.br/wp-content/uploads/2023/08/roberto-carlos-1.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://yt3.googleusercontent.com/ZImswuUeY9NmW7zmgBwW7j4IUVjJOlWSlHxG-1DyYv9-VkFFkMbamCf9LgrtF4VwIn6WCWY-KA=s900-c-k-c0x00ffffff-no-rj";
+}
+
